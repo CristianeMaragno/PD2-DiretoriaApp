d67f9a2a588990a7ae65bd99081d5b122665a855
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-07-23 16:31:38 -0300

    Layout e funcionalidade básicas da tela Users

--- app/src/main/java/com/cristianerm/pd2diretoria/UsersActivity.java
+++ app/src/main/java/com/cristianerm/pd2diretoria/UsersActivity.java
@@ -2,13 +2,52 @@ package com.cristianerm.pd2diretoria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class UsersActivity extends AppCompatActivity {
+
+    ImageButton voltar;
+    Button cadastrar_user;
+    Button remover_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        voltar = (ImageButton) findViewById(R.id.buttonVoltarUser);
+        cadastrar_user = (Button) findViewById(R.id.button_cadastrar_usuario);
+        remover_user = (Button) findViewById(R.id.button_remover_usuario);
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(UsersActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cadastrar_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(UsersActivity.this, UserCadastroActivity.class);
+                startActivity(i);
+            }
+        });
+
+        remover_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(UsersActivity.this, UserRemovalActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
