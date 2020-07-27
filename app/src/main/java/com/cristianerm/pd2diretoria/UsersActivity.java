32eeb93c61ac2ad184effbe2c6a90b7b091ff867
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-07-27 13:26:20 -0300

    Adiciona autenticação

--- app/src/main/java/com/cristianerm/pd2diretoria/UsersActivity.java
+++ app/src/main/java/com/cristianerm/pd2diretoria/UsersActivity.java
@@ -1,5 +1,6 @@
 package com.cristianerm.pd2diretoria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UsersActivity extends AppCompatActivity {
 
     ImageButton voltar;
     Button cadastrar_user;
     Button remover_user;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,22 @@ public class UsersActivity extends AppCompatActivity {
         voltar = (ImageButton) findViewById(R.id.buttonVoltarUser);
         cadastrar_user = (Button) findViewById(R.id.button_cadastrar_usuario);
         remover_user = (Button) findViewById(R.id.button_remover_usuario);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(UsersActivity.this, userID, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(UsersActivity.this, "User not sighed in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +74,19 @@ public class UsersActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
