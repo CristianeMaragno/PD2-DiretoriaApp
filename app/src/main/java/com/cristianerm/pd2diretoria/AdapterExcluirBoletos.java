99e69d06ec7a9ef1610e136e5112919be840d08b
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-08-06 16:50:23 -0300

    Deleta boleto do database(Referência) e do Storage(PDF)

--- app/src/main/java/com/cristianerm/pd2diretoria/AdapterExcluirBoletos.java
+++ app/src/main/java/com/cristianerm/pd2diretoria/AdapterExcluirBoletos.java
@@ -30,6 +30,7 @@ public class AdapterExcluirBoletos extends RecyclerView.Adapter<AdapterExcluirBo
     private ArrayList<ExcluirBoletosItem> mBoletosList;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
+    private DatabaseReference myRefDeleteBoleto;
 
 
     public static class AdapterExcluirBoletosViewHolder extends RecyclerView.ViewHolder {
@@ -65,18 +66,19 @@ public class AdapterExcluirBoletos extends RecyclerView.Adapter<AdapterExcluirBo
             public void onClick(View view) {
                 String boletoName = currentItem.getText();
                 String aluno = currentItem.getUserName();
-                ConfirmarDelete(aluno, boletoName, view);
+                String aluno_id = currentItem.getUserId();
+                ConfirmarDelete(aluno, boletoName, view, aluno_id);
             }
         });
     }
 
-    private void ConfirmarDelete(final String aluno, final String boletoName, View v){
+    private void ConfirmarDelete(final String aluno, final String boletoName, View v, final String aluno_id){
         new AlertDialog.Builder(v.getContext())
                 .setTitle("Comfirmação:")
                 .setMessage("Você tem certeza que deseja deletar este boleto?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        DeleteBoleto(aluno, boletoName);
+                        DeleteBoleto(aluno, boletoName, aluno_id);
                     }
                 })
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -88,25 +90,58 @@ public class AdapterExcluirBoletos extends RecyclerView.Adapter<AdapterExcluirBo
                 .show();
     }
 
-    private void DeleteBoleto(String aluno, String boletoName){
+    private void DeleteBoleto(String aluno, final String boletoName, final String aluno_id){
         String nome_boleto = aluno + "_" + boletoName + ".pdf";
         Log.d("Adapter Excluir Boletos", nome_boleto);
         String url_file = "gs://pd2apps.appspot.com/Boletos/"+nome_boleto;
-        Log.d("Adapter Excluir Boletos", url_file);
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReferenceFromUrl(url_file);
-
 
         storageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 Log.d("Adapter Excluir Boletos", "File deleted successfully");
+                DeleteBoletoFromDatabase(boletoName, aluno_id);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
                 // Uh-oh, an error occurred!
+                Log.d("Adapter Excluir Boletos", "File not deleted successfully");
+            }
+        });
+    }
+
+    private void DeleteBoletoFromDatabase(String boletoName, final String aluno_id){
+        String[] arr = boletoName.split(" ");
+        final String mes = arr[0];
+        final String ano = arr[1];
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference().child(aluno_id).child("boletos").child(ano);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ExcluirBoletosInformation bInfo = new ExcluirBoletosInformation();
+                    bInfo.setMes(ds.getValue(ExcluirBoletosInformation.class).getMes());
+
+                    String mes_database = bInfo.getMes();
+
+                    if(mes_database.equals(mes)){
+                        String database_key = ds.getKey();
+                        myRefDeleteBoleto = mFirebaseDatabase.getReference().child(aluno_id).child("boletos").child(ano).child(database_key);
+                        myRefDeleteBoleto.removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
