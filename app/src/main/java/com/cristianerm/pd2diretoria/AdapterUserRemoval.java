5f0b86ad30787a9cdcf25078fd548dfd84954c0f
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-08-10 19:19:37 -0300

    Muda o método e local de onde é recuperado o click na imagem de lixeira

--- app/src/main/java/com/cristianerm/pd2diretoria/AdapterUserRemoval.java
+++ app/src/main/java/com/cristianerm/pd2diretoria/AdapterUserRemoval.java
@@ -22,108 +22,23 @@ import java.util.ArrayList;
 public class AdapterUserRemoval extends RecyclerView.Adapter<AdapterUserRemoval.AdapterUserRemovalViewHolder> {
 
     private ArrayList<RemoverUserItem> mUsersList;
+    private FirebaseDatabase mFirebaseDatase;
+    private DatabaseReference myRef;
+    private DatabaseReference myRef2;
+    private DatabaseReference myRefDeleteUser;
+    private DatabaseReference myRefDeleteUserFromUsersGroup;
+    private DatabaseReference myRefDeleteUserFromUsers;
+    private DatabaseReference myRefPutUserOnDeletedList;
 
-    public static class AdapterUserRemovalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public static class AdapterUserRemovalViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTextView;
         public ImageView mImageView;
-
-        private FirebaseDatabase mFirebaseDatase;
-        private DatabaseReference myRef;
-        private DatabaseReference myRef2;
-        private DatabaseReference myRefDeleteUser;
-        private DatabaseReference myRefDeleteUserFromUsersGroup;
-        private DatabaseReference myRefDeleteUserFromUsers;
 
         public AdapterUserRemovalViewHolder(View itemView) {
             super(itemView);
             mTextView = itemView.findViewById(R.id.textViewUserRegistrado);
             mImageView = itemView.findViewById(R.id.imageViewDeleteUser);
-
-            mImageView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            String name_user = mTextView.getText().toString();
-            OpenConfirmarDelete(name_user, v);
-            Log.d("Adapter User Removal", "On click of image view!! Nome: " + name_user);
-        }
-
-        public void OpenConfirmarDelete(final String name_user, View v){
-
-            new AlertDialog.Builder(v.getContext())
-                    .setTitle("Comfirmação:")
-                    .setMessage("Você tem certeza que deseja deletar " + name_user + "?")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            GetUserInfo(name_user);
-                            //DeleteUser();
-                        }
-                    })
-                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            //Não fazer nada
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }
-
-        public void GetUserInfo(final String name_user){
-            mFirebaseDatase = FirebaseDatabase.getInstance();
-            myRef = mFirebaseDatase.getReference().child("user").child(name_user);
-
-            myRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        UserInformation uInfo = new UserInformation();
-                        uInfo.setStatus(ds.getValue(UserInformation.class).getStatus());
-
-                        final String user_status = uInfo.getStatus();
-
-                        DeleteUserInfoFromDatabase(name_user, user_status);
-                    }
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-        }
-
-        public void DeleteUserInfoFromDatabase(final String name_user, final String user_status){
-            myRef2 = mFirebaseDatase.getReference().child(user_status).child(name_user);
-
-            myRef2.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        UserInformation idInfo = new UserInformation();
-                        idInfo.setUser_id(ds.getValue(UserInformation.class).getUser_id());
-
-                        String user_id = idInfo.getUser_id();
-
-                        myRefDeleteUser = mFirebaseDatase.getReference().child(user_id);
-                        myRefDeleteUserFromUsersGroup = mFirebaseDatase.getReference().child(user_status).child(name_user);
-                        myRefDeleteUserFromUsers = mFirebaseDatase.getReference().child("user").child(name_user);
-
-                        myRefDeleteUser.removeValue();
-                        myRefDeleteUserFromUsersGroup.removeValue();
-                        myRefDeleteUserFromUsers.removeValue();
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
         }
 
     }
@@ -138,10 +53,97 @@ public class AdapterUserRemoval extends RecyclerView.Adapter<AdapterUserRemoval.
     }
     @Override
     public void onBindViewHolder(AdapterUserRemovalViewHolder holder, int position) {
-        RemoverUserItem currentItem = mUsersList.get(position);
+        final RemoverUserItem currentItem = mUsersList.get(position);
         holder.mImageView.setImageResource(currentItem.getImageResource());
         holder.mTextView.setText(currentItem.getText());
+
+        holder.mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_user = currentItem.getText();
+                String email_user = "vicentealves@gmail.com";
+                OpenConfirmarDelete(name_user, view);
+                Log.d("Adapter User Removal", "On click of image view!! Nome: " + name_user);
+            }
+        });
     }
+
+    public void OpenConfirmarDelete(final String name_user, View v){
+
+        new AlertDialog.Builder(v.getContext())
+                .setTitle("Comfirmação:")
+                .setMessage("Você tem certeza que deseja deletar " + name_user + "?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //GetUserInfo(name_user);
+                        //PutUserOnDeletedList();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Não fazer nada
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    public void GetUserInfo(final String name_user){
+        mFirebaseDatase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatase.getReference().child("user").child(name_user);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    UserInformation uInfo = new UserInformation();
+                    uInfo.setStatus(ds.getValue(UserInformation.class).getStatus());
+
+                    final String user_status = uInfo.getStatus();
+
+                    DeleteUserInfoFromDatabase(name_user, user_status);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void DeleteUserInfoFromDatabase(final String name_user, final String user_status){
+        myRef2 = mFirebaseDatase.getReference().child(user_status).child(name_user);
+
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    UserInformation idInfo = new UserInformation();
+                    idInfo.setUser_id(ds.getValue(UserInformation.class).getUser_id());
+
+                    String user_id = idInfo.getUser_id();
+
+                    myRefDeleteUser = mFirebaseDatase.getReference().child(user_id);
+                    myRefDeleteUserFromUsersGroup = mFirebaseDatase.getReference().child(user_status).child(name_user);
+                    myRefDeleteUserFromUsers = mFirebaseDatase.getReference().child("user").child(name_user);
+
+                    myRefDeleteUser.removeValue();
+                    myRefDeleteUserFromUsersGroup.removeValue();
+                    myRefDeleteUserFromUsers.removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     @Override
     public int getItemCount() {
         return mUsersList.size();
